3457271f75c34e32a883edc54494c6b4a8569e5f
Garbi93 <iseung2093@gmail.com>
2024-05-04 14:35:31 +0900

    5/04 수정/삭제 처리

--- src/components/common/ResultModal.js
+++ src/components/common/ResultModal.js
@@ -1,11 +1,5 @@
 import React from 'react';
 
-const initState = {
-    title: '',
-    content: '',
-    dueDate: '',
-}
-
 function ResultModal({title, content, callbackFn}) {
     return (
       <div className={`fixed top-0 left-0 z-[1055] flex h-full w-full justify-center bg-black bg-opacity-20`}>
--- src/pages/todo/ModifyPage.js
+++ src/pages/todo/ModifyPage.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import ModifyComponent from "../../components/todo/ModifyComponent";
+import {useParams} from "react-router-dom";
 
-function ModifyPage(props) {
-  return (
-    <div className="text-3xl font-extrabold">
-      Todo Modify Page
-    </div>
-  );
+function ModifyPage() {
+
+    const {tno} = useParams();
+
+    return (
+      <div className="p-4 w-full bg-white">
+          <div className="text-3xl font-extrabold">
+              Todo Modify Page
+          </div>
+          <ModifyComponent tno={tno}/>
+      </div>
+    );
 }
 
 export default ModifyPage;
