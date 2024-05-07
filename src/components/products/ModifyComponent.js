2b60b8b6447ec603e78b298382103f0053935877
Garbi93 <iseung2093@gmail.com>
2024-05-07 18:39:55 +0900

    5/07 상품 수정/삭제 컴포넌트 처리(2)

--- src/components/products/ModifyComponent.js
+++ src/components/products/ModifyComponent.js
@@ -1,7 +1,9 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {getOne} from "../../api/productsApi";
+import {deleteOne, getOne, putOne} from "../../api/productsApi";
 import FetchingModal from "../common/FetchingModal";
 import {API_SERVER_HOST} from "../../api/todoApi";
+import useCustomMove from "../../hooks/useCustomMove";
+import ResultModal from "../common/ResultModal";
 
 const initState = {
     pno: 0,
@@ -20,6 +22,10 @@ function ModifyComponent({pno}) {
     const [product, setProduct] = useState(initState);
 
     const [fetching, setFetching] = useState(false);
+
+    const [result, setResult] = useState(false);
+
+    const {moveToList, moveToRead} = useCustomMove();
 
     const uploadRef = useRef();
 
@@ -50,10 +56,65 @@ function ModifyComponent({pno}) {
 
     }
 
-    return (
-      <div className = "border-2 border-sky-200 mt-10 m-2 p-4">
+    const handleClickModify = () => {
 
-          {fetching? <FetchingModal/> :<></>}
+        const files = uploadRef.current.files;
+
+        const formData = new FormData();
+
+        for (let i = 0; i < files.length; i++) {
+            formData.append('files', files[i]);
+        }
+        formData.append("pname", product.pname);
+        formData.append("pdesc", product.pdesc);
+        formData.append("price", product.price);
+        formData.append("delFlag", product.delFlag);
+
+        for (let i = 0; i < product.uploadFileNames.length; i++) {
+
+            formData.append("uploadFileNames", product.uploadFileNames[i]);
+
+        }
+
+        setFetching(true);
+
+        putOne(pno, formData).then(data => {
+
+            setResult('Modified')
+            setFetching(false);
+
+        });
+    }
+
+    const handleClickDelete = () => {
+
+        setFetching(true);
+
+        deleteOne(pno).then(data => {
+            setResult("Deleted");
+            setFetching(false);
+        })
+
+    }
+
+    const closeModal = () => {
+        if (result === 'Modified') {
+            moveToRead(pno);
+        } else if (result === 'Deleted') {
+            moveToList({page: 1});
+        }
+        setResult(null);
+    }
+
+    return (
+      <div className="border-2 border-sky-200 mt-10 m-2 p-4">
+
+          {fetching ? <FetchingModal/> : <></>}
+
+          {result ? <ResultModal
+            title={`${result}`}
+            content={'처리 되었습니다.'}
+            callbackFn={closeModal}/> : <></>}
 
           <div className="flex justify-center">
               <div className="relative mb-4 flex w-full flex-wrap items-stretch">
@@ -63,7 +124,7 @@ function ModifyComponent({pno}) {
                     name="pname"
                     type={'text'}
                     value={product.pname}
-                    onChange={handleChangeProduct} ></input>
+                    onChange={handleChangeProduct}></input>
               </div>
           </div>
           <div className="flex justify-center">
@@ -120,21 +181,37 @@ function ModifyComponent({pno}) {
               <div className="relative mb-4 flex w-full flex-wrap items-stretch">
                   <div className="w-1/5 p-6 text-right font-bold">Images</div>
                   <div className="w-4/5 justify-center flex flex-wrap items-start">
-                      {product.uploadFileNames.map( (imgFile, i) =>
-                        <div className="flex justify-center flex-col w-1/3 m-1 align-baseline" key = {i}>
+                      {product.uploadFileNames.map((imgFile, i) =>
+                        <div className="flex justify-center flex-col w-1/3 m-1 align-baseline" key={i}>
                             <button
-                                className="bg-blue-500 text-3xl text-white"
-                                onClick={()=> deleteOldImages(imgFile)}
+                              className="bg-blue-500 text-3xl text-white"
+                              onClick={() => deleteOldImages(imgFile)}
                             >
                                 DELETE
                             </button>
                             <img
-                              alt ="img"
+                              alt="img"
                               src={`${host}/api/products/view/s_${imgFile}`}/>
                         </div>
                       )}
                   </div>
               </div>
+          </div>
+          <div className="flex justify-end p-4">
+              <button type="button"
+                      className="rounded p-4 m-2 text-xl w-32 text-white bg-red-500"
+                      onClick={handleClickDelete}>
+                  Delete
+              </button>
+              <button type="button" onClick={handleClickModify}
+                      className="inline-block rounded p-4 m-2 text-xl w-32 text-white bg-orange-500">
+                  Modify
+              </button>
+              <button type="button"
+                      className="rounded p-4 m-2 text-xl w-32 text-white bg-blue-500"
+                      onClick={() => moveToList()}>
+                  List
+              </button>
           </div>
       </div>
     );
