cefe0e54f80cb00f0347b057231d3cca660745ef
Garbi93 <iseung2093@gmail.com>
2024-05-07 14:46:35 +0900

    5/07 상품 목록 처리

--- src/router/productsRouter.js
+++ src/router/productsRouter.js
@@ -1,8 +1,22 @@
+import {lazy, Suspense} from "react";
+import {Navigate} from "react-router-dom";
 
+
+
+const Loading = <div className={"bg-orange-700"}>Loading ....</div>
+
+const ProductList = lazy(() => import("../pages/products/ListPage"));
 
 const productsRouter = () => {
     return [
-
+        {
+            path: 'list',
+            element: <Suspense fallback={Loading}><ProductList/></Suspense>
+        },
+        {
+            path: '',
+            element: <Navigate replace to={'/products/list'}></Navigate>
+        }
     ]
 }
 
