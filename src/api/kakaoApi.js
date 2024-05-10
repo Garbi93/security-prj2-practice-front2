87f0e30bbe7b9648eda41c7f4f69a983e97f9929
Garbi93 <iseung2093@gmail.com>
2024-05-10 14:41:05 +0900

    5/10 카카오 Access 토큰 받기

--- src/api/kakaoApi.js
+++ src/api/kakaoApi.js
@@ -1,8 +1,12 @@
+import axios from "axios";
 
 
 const rest_api_key = process.env.REACT_APP_Kakao_Rest_Api_Key;
 const redirect_uri = process.env.REACT_APP_Redirect_Uri;
 const auth_code_path = process.env.REACT_APP_KaKao_Code_Path;
+
+const access_token_url = process.env.REACT_APP_Kakao_Access_Token_Url;
+
 
 export const getKakaoLoginLink = () => {
 
@@ -11,3 +15,27 @@ export const getKakaoLoginLink = () => {
     return kakaoURL;
 
 }
+
+export const getAccessToken = async (authCode) => {
+
+    const header = {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+    };
+
+    const params = {
+        grant_type: "authorization_code",
+        client_id: rest_api_key,
+        redirect_uri: redirect_uri,
+        code:authCode
+    }
+
+    const res = await axios.post(access_token_url, params, header);
+
+    const accessToken = res.data.access_token;
+
+    return accessToken;
+
+}
+
--- src/pages/member/KakaoRedirectPage.js
+++ src/pages/member/KakaoRedirectPage.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useSearchParams} from "react-router-dom";
+import {getAccessToken} from "../../api/kakaoApi";
 
 function KakaoRedirectPage(props) {
 
     const [searchParams] = useSearchParams();
 
     const authCode = searchParams.get('code');
+
+    useEffect(() => {
+
+        getAccessToken(authCode).then(data => {
+            console.log(data);
+        })
+
+    }, [authCode]);
 
     return (
       <div>
