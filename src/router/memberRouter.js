4b6b7f96be06b982793dfe2e059b7beaac3e2916
Garbi93 <iseung2093@gmail.com>
2024-05-10 17:51:37 +0900

    5/10 리엑트 사용자 정보 수정

--- src/router/memberRouter.js
+++ src/router/memberRouter.js
@@ -6,6 +6,7 @@ const Loading = <div className={"bg-orange-700"}>Loading ....</div>
 const Login = lazy(() => import("../pages/member/LoginPage"));
 const Logout = lazy(() => import("../pages/member/LogoutPage"));
 const KakaoRedirect = lazy(() => import("../pages/member/KakaoRedirectPage"));
+const MemberModify = lazy(() => import("../pages/member/ModifyPage"));
 
 
 const memberRouter = () => {
@@ -19,6 +20,9 @@ const memberRouter = () => {
         },{
             path: "kakao",
             element: <Suspense fallback={Loading}><KakaoRedirect/></Suspense>
+        },{
+            path: "modify",
+            element: <Suspense fallback={Loading}><MemberModify/></Suspense>
         }
     ]
 }
