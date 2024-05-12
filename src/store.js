3e603ac340c3d027fad5513df1fd28480fe6f410
Garbi93 <iseung2093@gmail.com>
2024-05-12 15:30:06 +0900

    5/12 cartSlice 작성

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import loginSlice from "./slices/loginSlice";
+import cartSlice from "./slices/cartSlice";
 
 // store 는 금고 라고 생각 하고
 export default configureStore({
     // reducer 에서 파트가 나뉘어 진다.
     reducer: {
         "loginSlice": loginSlice,
+        "cartSlice": cartSlice
     }
 });
